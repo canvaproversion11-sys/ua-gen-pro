68a795bde9bc809a08828807fe1ee779e230c487
canvaproversion11-sys <canvaproversion11@gmail.com>
2025-09-10 12:26:00 +0600

    Updated app-build-manifest.json

---
+++ .next/static/chunks/app/loading.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/loading"],{
+
+/***/ 3521:
+/*!*******************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?server=false! ***!
+  \*******************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["vendors-node_modules_a","vendors-node_modules_next-","vendors-node_modules_next_dist_client_a","vendors-node_modules_next_dist_client_components_b","vendors-node_modules_next_dist_client_components_layout-router_js-4906aef6","vendors-node_modules_next_dist_client_components_m","vendors-node_modules_next_dist_client_components_p","vendors-node_modules_next_dist_client_components_react-dev-overlay_internal_components_C","vendors-node_modules_next_dist_client_components_react-dev-overlay_internal_components_O","vendors-node_modules_next_dist_client_components_react-dev-overlay_internal_c","vendors-node_modules_next_dist_client_components_react-dev-overlay_internal_h","vendors-node_modules_next_dist_client_components_react-dev-overlay_internal_styles_B","vendors-node_modules_next_dist_client_components_rea","vendors-node_modules_next_dist_client_components_re","vendors-node_modules_next_dist_client_components_router-reducer_p","vendors-node_modules_next_dist_client_c","vendors-node_modules_next_dist_client_l","vendors-node_modules_next_dist_compiled_a","vendors-node_modules_next_dist_compiled_m","vendors-node_modules_next_dist_compiled_react-dom_cjs_react-dom_development_js-3041f41d","vendors-node_modules_next_dist_compiled_react-d","vendors-node_modules_next_dist_compiled_react_c","vendors-node_modules_next_dist_compiled_r","vendors-node_modules_next_d","vendors-node_modules_radix-ui_react-d","vendors-node_modules_ra","vendors-node_modules_styled-jsx_d","vendors-node_modules_supabase_a","vendors-node_modules_su","vendors-node_modules_tailwind-merge_dist_bundle-mjs_mjs-a19ea93e","vendors-node_modules_t","main-app"], function() { return __webpack_exec__(3521); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
